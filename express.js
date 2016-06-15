09f32b7f971e428f5c5167141b0b53795c623cc3
javoche <arancibiajav@gmail.com>
2016-06-15 10:17:41 +0000

    appDetails get endpoint

--- express.js
+++ express.js
@@ -7,6 +7,28 @@ var https = require('https');
 var http = require('http');
 var fs = require('fs');
 
+var appDetails = {};
+
+function appDetailsFill() {
+	try {
+		var data = JSON.parse(fs.readFileSync(process.cwd() + '/package.json'));
+		Object.assign(appDetails, {
+			name: data.name,
+			version: data.version,
+			description: data.description,
+			author: data.author
+		});
+	}
+	catch (e) {
+
+	}
+}
+appDetailsFill();
+
+app.get('/appDetails', function(req, res) {
+	res.json(appDetails);
+});
+
 var PROD = process.env.PROD && process.env.PROD.toString() == '1' || false;
 
 if (!PROD) {
