ab0364dd3bdc6357fad763a1aa06506cd75a6983
javoche <arancibiajav@gmail.com>
2017-01-09 15:02:26 +0000

    added-is-valid-to-rules-id

--- model/db.actions.js
+++ model/db.actions.js
@@ -425,7 +425,11 @@ exports.create = function(modelName, m) {
                 }
             }
             else {
-                if(x=='_d'){
+                if(x.toString().toLowerCase()=='_id'){
+                    
+                    var isValid = mongoose.Types.ObjectId.isValid(data[x]);
+                    console.log('_ID ',data[x],'isValid:',isValid);
+                    
                     data[x] = mongoose.Types.ObjectId(data[x]);
                 }
                 rules[x] = {
