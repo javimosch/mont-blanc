ae83e589375dda00b8c85392b2dc03df945b2b29
javoche <arancibiajav@gmail.com>
2017-01-12 11:00:33 +0000

    working-new-building-type

--- public/js/services/order-question.js
+++ public/js/services/order-question.js
@@ -21,12 +21,10 @@
                 }
             };
 
-            function toggleMandatory(n, val) {
+            function setMandatory(n, val) {
                 s.diags.forEach((diag) => {
                     if ((n && diag.name == n) || !n) {
                         diag.mandatory = val;
-                        //console.log('toggle-mandatory ',n,val);
-                        r.dom();
                     }
                 });
             }
@@ -50,6 +48,52 @@
             s.$watch('item.info.gasInstallation', updateChecks);
             s.$watch('item.address', updateChecks);
             s.$watch('item.info.electricityInstallation', updateChecks);
+            
+            function dataRootExists(){
+                return s.item && s.item.info;
+            }
+            
+            function questionHasAValue(propertyName){
+                return dataRootExists() && s.item.info[propertyName];
+            }
+            
+            function isTruth(propertyName){
+                return questionHasAValue(propertyName) && s.item.info[propertyName]==true;
+            }
+            
+            function isSelling(){
+                return isTruth('sell');
+            }
+            
+            function selectDiagType(diagName,bool){
+                if(!s.item) return;
+                s.item.diags[diagName] = bool;
+            }
+            
+            function setMandatoryTruthAndSelect(diagName){
+                setMandatory(diagName,true);
+                if(dataRootExists()){
+                    selectDiagType(diagName,true);
+                }
+            }
+            
+            s.$watch('item.info.buildingType', onBuildingTypeChange);
+            function onBuildingTypeChange(buildingType){
+                if(!questionHasAValue('buildingType')) return;
+                if(buildingType==BUILDING_TYPE.OFFICE){
+                    if(isSelling()){
+                        //Amiante / Termites / Carrez / DPE / ERNMT
+                        setMandatory('dpe',true);
+                        setMandatory('ernt',true);
+                        setMandatory('carrez',true);
+                    }else{
+                        //Amiante / DPE / ERNMT
+                        setMandatory('dpe',true);
+                        setMandatory('ernt',true);
+                        setMandatory('carrez',false);
+                    }
+                }
+            }
 
             function updateChecks() {
 
@@ -59,58 +103,58 @@
                 if (s.item.info.constructionPermissionDate === 'Avant le 01/01/1949') {
                     toggle('crep', true);
                     s.item.diags.crep = true; //mandatory
-                    toggleMandatory('crep', true);
+                    setMandatory('crep', true);
                 } else {
                     s.item.diags.crep = false; //
                     toggle('crep', true);
-                    toggleMandatory('crep', false);
+                    setMandatory('crep', false);
                 }
 
                 if (departmentHasTermites() && s.item.info.sell) {
                     //toggle('termites', true);
                     s.item.diags.termites = true;
-                    toggleMandatory('termites', true);
+                    setMandatory('termites', true);
                 } else {
                     toggle('termites', false);
                     s.item.diags.termites = false;
-                    toggleMandatory('termites', false);
+                    setMandatory('termites', false);
                 }
 
                 if (_.includes(['Avant le 01/01/1949', 'Entre 1949 et le 01/07/1997'], s.item.info.constructionPermissionDate)) {
                     toggle('dta', true);
                     s.item.diags.dta = true; //mandatory
-                    toggleMandatory('dta', true);
+                    setMandatory('dta', true);
                 } else {
                     toggle('dta', true);
                     s.item.diags.dta = false;
-                    toggleMandatory('dta', false);
+                    setMandatory('dta', false);
                 }
 
                 if (_.includes(['Oui, Plus de 15 ans', 'Oui, Moins de 15 ans'], s.item.info.gasInstallation)) {
                     toggle('gaz', true);
                     if (s.item.info.sell == true && s.item.info.gasInstallation === 'Oui, Plus de 15 ans') {
                         s.item.diags.gaz = true;
-                        toggleMandatory('gaz', true);
+                        setMandatory('gaz', true);
                     } else {
                         s.item.diags.gaz = false;
-                        toggleMandatory('gaz', false);
+                        setMandatory('gaz', false);
                     }
                 } else {
                     toggle('gaz', false);
-                    toggleMandatory('gaz', false);
+                    setMandatory('gaz', false);
                 }
                 if (_.includes(['Plus de 15 ans', 'Moins de 15 ans'], s.item.info.electricityInstallation)) {
                     toggle('electricity', true);
                     if (s.item.info.sell == true && s.item.info.electricityInstallation === 'Plus de 15 ans') {
                         s.item.diags.electricity = true;
-                        toggleMandatory('electricity', true);
+                        setMandatory('electricity', true);
                     } else {
                         s.item.diags.electricity = false;
-                        toggleMandatory('electricity', false);
+                        setMandatory('electricity', false);
                     }
                 } else {
                     toggle('electricity', false);
-                    toggleMandatory('electricity', false);
+                    setMandatory('electricity', false);
                 }
 
             }
