32b8e31b190bcccc16901c00bcd91278f0bf00f8
javoche <arancibiajav@gmail.com>
2017-02-24 11:48:13 +0000

    xxx-iframe-prod-fix

--- tools/static-generator/sg-index.js
+++ tools/static-generator/sg-index.js
@@ -1,7 +1,7 @@
 var path = require('path');
 var Promise = require(path.join(process.cwd(), 'model/utils')).promise;
 var heTpls = require('./sg-templates');
-
+var sander = require('sander');
 module.exports = {
     configure: configure
 }
@@ -12,12 +12,17 @@ function configure(app) {
         heTpls.build().then(() => {
             console.log('DEBUG: main build all success at ' + new Date());
 
-            app.get('/js/bundle_1.js', function(req, res) {
-                res.sendFile(path.join(process.cwd(), 'static-generator/output/js/bundle_1.js'));
+
+            var files = sander.readdirSync(path.join(process.cwd(), 'static-generator/output/js'));
+            
+
+            files.forEach((fileName) => {
+                console.log('DEBUG SG-INDEX route', fileName);
+                app.get('/js/'+fileName, function(req, res) {
+                    res.sendFile(path.join(process.cwd(), 'static-generator/output/js/'+fileName));
+                });
             });
-            app.get('/js/vendor.js', function(req, res) {
-                res.sendFile(path.join(process.cwd(), 'static-generator/output/js/vendor.js'));
-            });
+
             app.get('/css/vendor.css', function(req, res) {
                 res.sendFile(path.join(process.cwd(), 'static-generator/output/css/vendor.css'));
             });
