47998a12d4eaca77c8f35596ba807e93aebccd52
javoche <arancibiajav@gmail.com>
2017-03-03 12:11:38 +0000

    xxx-deploy-script-update

--- tools/scripts/ssh_deploy_tag.sh
+++ tools/scripts/ssh_deploy_tag.sh
@@ -2,9 +2,10 @@
 . $(dirname "$0")/common_fn.sh
 printf "Gitlab release tag (EX: v1.1.4, empty string will deploy master branch) :"
 read tag
-printf "Enviroment (preprod or prod) :"
-read enviroment
+#//since 1.3.0 should be new
+#printf "Enviroment (preprod or prod) :"
+#read enviroment
 printf "SSH PWD: "
 read_secret password
-SSH_PWD=$password node -e "require('./utils/ssh').deployTag('$tag','$enviroment')"
+SSH_PWD=$password node -e "require('./utils/ssh').deployTag('$tag','new')"
 
