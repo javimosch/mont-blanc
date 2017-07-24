084aaa6e1499a9ddf5c1f58fa9baf8883927549d
Javier Leandro Arancibia <arancibiajav@gmail.com>
2017-07-24 22:03:03 +0000

    399 cors origin

--- model/middlewares/cors-middleware.js
+++ model/middlewares/cors-middleware.js
@@ -1,17 +1,35 @@
+var path = require('path');
+var resolver = require(path.join(process.cwd(), 'model/facades/resolver-facade'));
+var logger = resolver.loggerFacade({
+    name: "MIDDLEWARE",
+    category: "CORS"
+});
 module.exports = {
     bind: (app) => {
-        app.use(function(req, res, next) {
-            res.header("Access-Control-Allow-Origin", "*");
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-            next();
-        });
+        if (resolver.env().DISABLE_CORS) {
+            logger.debug('DISABLED');
+            //return;
+        }
 
-        //CORS
+        var origin = process.env.CORS_ORIGIN || "*";
+        logger.debugTerminal('Origin is', origin);
+
         app.all('*', function(req, res, next) {
             //console.log(req.method);
-            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Origin", origin);
             res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,POST,PUT,DELETE');
             res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+
+            if (!req.headers.origin) {
+                logger.warn('Anhautorized request', req.headers, req.url);
+                return res.send(401);
+            }else{
+                if(origin.indexOf(req.headers.origin)==-1){
+                    logger.warn('Anhautorized request', req.headers, req.url);
+                }
+            }
+
             if ('OPTIONS' == req.method) {
                 return res.send(200);
             }
