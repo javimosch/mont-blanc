458bec01c37f569fd1c0bdf6ee10b9f2e6c8ef3f
Javier Leandro Arancibia <arancibiajav@gmail.com>
2017-08-11 11:43:04 +0000

    XXX preprod login fix

--- public/js/vendor/vendor-check-preprod-session.js
+++ public/js/vendor/vendor-check-preprod-session.js
@@ -1,38 +1,69 @@
 (function() {
-    function urlHas(arr){
-        for(var x in arr){
-            if(window.location.href.indexOf(arr[x])!==-1) return true;
+    function urlHas(arr) {
+        for (var x in arr) {
+            if (window.location.href.indexOf(arr[x]) !== -1) return true;
         }
         return false;
     }
-    if (!urlHas(['herokuapp',"62.210.97.81"])) return;
+    if (!urlHas(['herokuapp', "62.210.97.81", 'c9'])) return;
+
+    var postToken;
+    apiGet("CXNlcg", function(res) {
+        res = JSON.parse(res.responseText);
+        postToken = res.result;
+        console.info('(preprod-check)', postToken);
+        checkSession();
+    });
 
     function decode() {
-        var encoded = localStorage.getItem('store#diags-preview-session') || '';
-        if (encoded) return JSON.parse(atob(encoded));
+        var encoded = window.localStorage.getItem('store#diags-preview-session') || '';
+        if (encoded) return JSON.parse(window.atob(encoded));
         else return null;
+    }
+
+    function setPostHeaders(x) {
+        x.setRequestHeader("Content-Type", "application/json");
+        x.setRequestHeader("csrf-token", postToken);
+        return x;
+    }
+
+
+    function apiGet(url, callback) {
+        var x = window.XMLHttpRequest && new window.XMLHttpRequest() || new window.ActiveXObject("Microsoft.XMLHTTP");
+        x.open('get', window.location.origin + '/' + url, true);
+        x.onreadystatechange = function() {
+            if (x.readyState == 4) {
+                callback(x);
+            }
+        };
+        x.setRequestHeader("Content-Type", "application/json");
+        x.send();
+    }
+
+    function checkId(_id, callback) {
+        var x = window.XMLHttpRequest && new window.XMLHttpRequest() || new window.ActiveXObject("Microsoft.XMLHTTP");
+        x.open('post', window.location.origin + '/api/' + window.btoa('User') + '/' + window.btoa('exists'), true);
+        x.onreadystatechange = function() {
+            if (x.readyState == 4) {
+                callback(x.responseText);
+            }
+        };
+        x = setPostHeaders(x);
+        x.send(JSON.stringify({
+            p: window.btoa(JSON.stringify({
+                _id: _id
+            }))
+        }));
     }
 
     function validSession(validSessionCallback) {
         var session = decode();
         if (session && session._id) {
-            //check _id
-            function checkId(_id, callback) {
-                var x = window.XMLHttpRequest && new window.XMLHttpRequest() || new ActiveXObject("Microsoft.XMLHTTP");
-                x.open('post', window.location.origin + '/api/'+btoa('User')+'/'+btoa('exists'), true);
-                x.onreadystatechange = function() {
-                    if (x.readyState == 4) {
-                        callback(x.responseText)
-                    }
-                };
-                x.setRequestHeader("Content-Type", "application/json");
-                x.send(JSON.stringify({
-                    _id: _id
-                }))
-            };
+
 
             return checkId(session._id, function(res) {
                 res = JSON.parse(res);
+                res.result = JSON.parse(window.atob(res.result));
                 if (!res.result) return validSessionCallback(false);
                 var now = Date.now();
                 var offset = 1000 * 60 * 20;
@@ -49,13 +80,16 @@
         }
         return validSessionCallback(false);
     }
-    validSession(function(valid) {
-        if (!valid) {
-            localStorage.setItem('store#diags-preview-session', '');
-            window.location.href = window.location.origin + '/preprod.html';
-        }
-        else {
-            console.info('this is a prepod valid session');
-        }
-    })
+
+    function checkSession() {
+        validSession(function(valid) {
+            if (!valid) {
+                window.localStorage.setItem('store#diags-preview-session', '');
+                window.location.href = window.location.origin + '/preprod.html';
+            }
+            else {
+                console.info('this is a prepod valid session');
+            }
+        })
+    }
 })();
